4b8c80fdf302a4546d580ccf6cb3ecabd0479ac8
tomi <tomi@x1carbon7th.local>
2024-09-16 02:37:32 +0900

    added Link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import * as React from "react"
+// import * as React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
+
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Gatsby!" />
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)

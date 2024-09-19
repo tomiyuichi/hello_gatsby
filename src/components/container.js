f49de355a2b7f08696f1efa12f034cac64e51235
tomi <tomi@x1carbon7th.local>
2024-09-20 02:30:11 +0900

    [styling] css-module 2

--- src/components/container.js
+++ src/components/container.js
@@ -1,5 +1,9 @@
 import React from "react"
 import * as containerStyles from "./container.module.css"
-export default ({ children }) => (
+// export default ({ children }) => (
+//   <div className={containerStyles.container}>{children}</div>
+// )
+const Container = ({ children }) => (
   <div className={containerStyles.container}>{children}</div>
 )
+export default Container
